5e75e09581bc4461dad8eee25d86146621ddd670
ender-titan1 <supszym02@gmail.com>
2021-12-24 00:12:10 +0100

    Start to add low HP vignette animation

---
+++ Moondown/Assets/Scripts/Graphics/PostProcessingAnimationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Moondown.Graphics.Animation
+{
+    public class PostProcessingAnimationController : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Animator animator = GameObject.FindGameObjectWithTag("LowHealthVignette").GetComponent<Animator>();
+            
+            
+        }
+    }
+}
