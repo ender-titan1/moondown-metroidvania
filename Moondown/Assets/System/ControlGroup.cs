502535b34577c082a41d4815fe19c99f5ffa94fd
ender-titan1 <supszym02@gmail.com>
2022-06-08 22:00:50 +0200

    add: Begin work on ControlGroupFactory

---
+++ Moondown/Assets/System/ControlGroup.cs
@@ -0,0 +1,45 @@
+﻿/*
+    Copyright (C) 2021 Moondown Project
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Moondown.Sys
+{
+    public struct ControlGroup
+    {
+        public enum Task
+        {
+            Idle,
+            Wandering,
+            Collecting,
+            Defending,
+            Protecting,
+            Scouting,
+            Repairing,
+            Scavenging,
+            Reasearching
+        }
+
+        // TEMPORARY
+        public UnitData[] units;
+        public Task task;
+
+        public ControlGroup(UnitData[] units, Task task)
+        {
+            this.units = units;
+            this.task = task;
+        }
+    }
+}
---
+++ Moondown/Assets/System/UnitData.cs
@@ -0,0 +1,74 @@
+﻿/*
+
+    Copyright (C) 2021 Moondown Project
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Moondown.Sys
+{
+    public struct UnitData
+    {
+        public enum Behaviour
+        {
+            Regular,
+            Broken,
+            Agitated,
+            Cautious,
+            Ancient,
+            Oblivious
+        }
+
+        public enum Focus
+        {
+            RangeFocused,
+            MeleeFocused,
+            SupportFocused,
+            Neutral
+        }
+
+        public enum Aesthetics
+        {
+            Clean,
+            Dusty,
+            Sandy,
+            Dirty,
+            Worn,
+            Clan,
+            Frozen
+        }
+        
+        public enum Physical
+        {
+            Regular,
+            Armoured,
+            Exposed,
+            Weakened,
+            Clan
+        }
+
+        public Behaviour behaviour;
+        public Focus focus;
+        public Aesthetics aesthetics;
+        public Physical physical;
+
+        public UnitData(Behaviour b, Focus f, Aesthetics a, Physical p)
+        {
+            behaviour = b;
+            focus = f;
+            aesthetics = a;
+            physical = p;
+        }
+    }    
+}
